17c77a34edda4d054d541c8c963c5d9d3347cd04
Toshimonster <joshuapetercooper@hotmail.co.uk>
2017-06-27 15:28:17 +0100

    Delete Testcommand3.py

--- filestoupload/Testcommand3.py
+++
@@ -1,9 +0,0 @@
-
-    {
-        "alias" : ['running'],
-        "execute" : """
-sendMessage(channel, mention(user) + 'https://i.kinja-img.com/gawker-media/image/upload/s--5Wj0lBIo--/c_scale,f_auto,fl_progressive,q_80,w_800/nomefdcma7s1pctiqaad.gif RUNNING IN THE 90s')
-"""
-        }
-    
-
