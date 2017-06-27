8f2060f7680f4a05dfa2dd02c71e25c569870507
Toshimonster <joshuapetercooper@hotmail.co.uk>
2017-06-27 10:30:53 +0100

    Delete Testcommand.py

--- Testcommand.py
+++
@@ -1,9 +0,0 @@
-
-    {
-        "alias" : ['shouldnt', 'notworking', 'impossible', 'snoop'],
-        "execute" : """
-sendMessage(channel, mention(user) + ':rr: This shouldn't work, but it does: https://www.youtube.com/watch?v=9w_2JGILUoc')
-"""
-        }
-    
-
