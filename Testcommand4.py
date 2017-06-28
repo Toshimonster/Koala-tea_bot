ebf98005c82643143fa841864dcc557c36458482
RRTemp <Twexp77@gmail.com>
2017-06-28 09:36:38 +0100

    Add files via upload

---
+++ Testcommand4.py
@@ -0,0 +1,9 @@
+
+    {
+        "alias" : ['sekrit', 'secret', 'dokuments', 'documents'],
+        "execute" : """
+sendMessage(channel, mention(user) + http://cdn-live.warthunder.com/uploads/f4/9b8f9593601926568cc98120ded45b1dc588f3_lq/Sekrit%20dokuments%20wip.PNG Here are sekrit dokuments for you.
+"""
+        }
+    
+
