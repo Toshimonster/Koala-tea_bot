11bd2ea5eab9563e3bcad3b736b30174e62d5e79
RRTemp <Twexp77@gmail.com>
2017-06-28 09:41:13 +0100

    Add files via upload

---
+++ uploads/Testcommand4.py
@@ -0,0 +1,13 @@
+
+    {
+        "name" : Sekrit Dokuments
+        "description" : This will show true russian sekrit dokuments cover.
+        "hidden" : True
+        "syntax" : "Help (command)",
+        "alias" : ['sekrit', 'secret', 'dokuments'],
+        "execute" : """
+sendMessage(channel, mention(user) + http://cdn-live.warthunder.com/uploads/f4/9b8f9593601926568cc98120ded45b1dc588f3_lq/Sekrit%20dokuments%20wip.PNG Here are sekrit dokuments for you.
+"""
+        }
+    
+
