b82250211992dc69c9deaf8213d8e494f8eb2617
Toshimonster <joshuapetercooper@hotmail.co.uk>
2017-06-30 13:56:32 +0100

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -9,15 +9,15 @@ settings = {
     "TEATIME_LENGTH" : 180,
 
     #The bot token for the bot
-    "SLACK_BOT_TOKEN" : os.getenv('SLACK_BOT_TOKEN'),
+    "SLACK_BOT_TOKEN" : '',
 
     #The imgur api infomation for the !img command
-    "IMGUR_BOT_ID" : os.getenv('IMGUR_BOT_ID'),
+    "IMGUR_BOT_ID" : '',
 
     #The imgur api infomation for the !img command
-    "IMGUR_BOT_SECRET" : os.getenv('IMGUR_BOT_SECRET'),
+    "IMGUR_BOT_SECRET" : '',
 
     #The tenor api infomation for the !gif command
-    "TENOR_KEY" : os.getenv('TENOR_KEY')
+    "TENOR_KEY" : ''
 
 }
